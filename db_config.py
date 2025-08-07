8c5081adf69019bd26a6b5950a6854f44d81c104
Anne karoline Gomes <annekarolinegomes875@gmail.com>
2025-08-07 14:25:55 +0000

    criação de conexão com mysql

---
+++ db_config.py
@@ -0,0 +1,19 @@
+# db_config.py
+
+import mysql_connector
+from mysql_connector import Error
+
+def conectar():
+    try:
+        conexao = mysql_connector.connect(
+          host='localhost:3306',
+          user='root',   # Troque se necessário
+          password='eec123456@#$',
+          database='sgb'  # Nome do seu banco
+    )
+        if conexao.is_connected():
+          print("Conexão bem-sucedida com o banco de dado!")
+          return conexao
+    except Error as e:
+        print(f'Erro ao conectar ao banco de dado: {e}')
+        return None
