e0cd8cbc8a77a7da84312072a4599d2963148529
Anne karoline Gomes <annekarolinegomes875@gmail.com>
2025-08-13 19:41:32 +0000

    criação do cruds restants das demais tabelas do banco de dados dentro do python

--- crud/aluno.py
+++ crud/aluno.py
@@ -66,9 +66,31 @@ def atualizar_aluno(id_aluno, nome=None, email=None, senha=None, serie=None,  st
         if status is not None:
             campos.append("status=%s"); valores.append(status)
         if not campos:
-            return {"ststus":"aviso","mensagem":"Nada para atualizar."}
+            return {"status":"aviso","mensagem":"Nada para atualizar."}
         sql = "UPDATE Aluno SET " + ", ".join(campos) + "WHERE id=%s"
         valores.append(id_aluno)
         cursor.execute(sql, tuple(valores))
         conn.commit()
-        if cursor.rowcount==0
+        if cursor.rowcount==0:
+            return {"status":"aviso","mensagem":"Aluno não encontrado para atualizar."}
+        return {"status":"sucesso","mensagem":"Aluno atualizado."}
+    except Exception as e:
+        return {"status":"erro","mensagem":str(e)}
+    finally:
+        try:
+        except: pass
+
+def deletar_aluno(id_aluno):
+    try:
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Aluno WHERE id=%s", (id_aluno,))
+        conn.commit()
+        if cursor.rowcount==0:
+            return {"status":"aviso","mensagem":"Aluno não encontrado."}
+        return  {"status":"sucesso","mensagem":"Aluno excluído."}
+    except Exception as e:
+        return {"status":"erro","mensagem":str(e)}
+    finally:
+        try: conn.close()
+        except: pass
